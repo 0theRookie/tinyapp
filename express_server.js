edeee4bfa3d4d09dba4b408564bae26d17d7e4c1
Keishon Ross <keishonross97@gmail.com>
2018-10-30 20:12:16 +0000

    installed and required express in project folder, set port and created urlDatabase

---
+++ express_server.js
@@ -0,0 +1,9 @@
+const express = require('express');//requiring express
+const app = express();
+const PORT = 8080;//setting port to listen on
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+}
+
