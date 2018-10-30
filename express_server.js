1169dbf7fbe093a3dd06c2ca21dae98666ef0c4e
Keishon Ross <keishonross97@gmail.com>
2018-10-30 20:19:44 +0000

    added server listener (on port 8080)

--- express_server.js
+++ express_server.js
@@ -7,7 +7,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
+//GET request handler - takes in request and responds with anonymous cb func
 app.get("/", (request, response) => {
   response.send("Hello traveller!")
 })
 
+//Server listener - waits for request and responds
+app.listen(PORT, () => {
+  console.log(`App listening on port : ${PORT}!`)
+})
